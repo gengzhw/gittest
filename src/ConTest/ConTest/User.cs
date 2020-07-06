7668951641406890801ea9e55cada4422725f429
gengzhw <1828489@qq.com>
2020-07-07 06:00:02 +0800

    修改代码增加user

---
+++ src/ConTest/ConTest/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConTest
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Sex { get; set; }
+
+        public override string ToString()
+        {
+            return $@"{Name} {Sex}";
+        }
+    }
+}
